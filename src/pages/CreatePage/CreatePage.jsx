29e9aa1e92b901e008b7f3dd50209b34f0f406b3
Ramees <rameesc985@gmail.com>
2024-12-13 16:18:09 +0530

    pages structure created

---
+++ src/pages/CreatePage/CreatePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/Header/Header';
+import Create from '../../components/Create/Create'
+
+function CreatePage() {
+  return (
+    <>
+    <Header />
+    <Create />
+    </>
+  )
+}
+
+export default CreatePage
---
+++ src/pages/ViewPost/ViewPost.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import View from '../../components/View/View'
+
+function ViewPost() {
+  return (
+  <>
+   <Header />
+   <View />
+  </>
+  )
+}
+
+export default ViewPost
