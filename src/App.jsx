55c9cf250adcaeec7562404818ea62d5892e39d9
Ramees <rameesc985@gmail.com>
2024-12-13 16:10:03 +0530

    router dom implimented, create, login, signup, view component disigned

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import Banner from "./components/Banner/Banner"
-import Footer from "./components/Footer/Footer"
-import Header from "./components/Header/Header"
-import Posts from "./components/Posts/Posts"
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Banner />
-      <Posts />
-      <Footer />
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+    </>
   )
 }
 
