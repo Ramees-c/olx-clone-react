f667456c9ef0fe75c78a64f2b453cd804fac7f43
Ramees <rameesc985@gmail.com>
2024-12-13 11:35:44 +0530

    header and banner design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Banner from "./components/Banner/Banner"
+import Header from "./components/Header/Header"
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Header />
+      <Banner />
+    </div>
   )
 }
 
