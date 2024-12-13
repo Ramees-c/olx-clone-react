7b87bb86f58ac9052f3d8d16d2a2596dbe419d11
Ramees <rameesc985@gmail.com>
2024-12-13 11:45:13 +0530

    posts and footer design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Banner from "./components/Banner/Banner"
+import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
+import Posts from "./components/Posts/Posts"
 
 function App() {
   return (
     <div>
       <Header />
       <Banner />
+      <Posts />
+      <Footer />
     </div>
   )
 }
